da66280240eed3d2520da9c72f26cf6cd8ecfc86
Horem <horemvore@live.co.uk>
2013-09-21 16:02:36 +0100

    Unit Art Defines sql update

--- CCTP/Buildings/Civ5Buildings.sql
+++ CCTP/Buildings/Civ5Buildings.sql
@@ -1,25 +1,11 @@
-
 -- Change free BuildingClass from Palace to Chieftain's Hut
-
-UPDATE Civilization_FreeBuildingClasses 
-SET BuildingClassType='BUILDINGCLASS_TREE'
-WHERE BuildingClassType='BUILDINGCLASS_PALACE';
-
+UPDATE Civilization_FreeBuildingClasses SET BuildingClassType = 'BUILDINGCLASS_TREE' WHERE BuildingClassType = 'BUILDINGCLASS_PALACE';
 -- Change PrereqTechs for Palace, Shrine and Monument BuildingClasses:
-
-UPDATE Buildings
-SET PrereqTech='TECH_LEADERSHIP', Cost='40'
-WHERE BuildingClass='BUILDINGCLASS_PALACE';
-
-
+UPDATE Buildings SET PrereqTech = 'TECH_LEADERSHIP' AND Cost = 40 WHERE BuildingClass = 'BUILDINGCLASS_PALACE';
 -- Raise/add Flavors for the Palace to force AI to build it:
+UPDATE Building_Flavors SET Flavor = 150 WHERE BuildingType = 'BUILDING_PALACE';
 
-UPDATE Building_Flavors
-SET Flavor='150'
-WHERE BuildingType='BUILDING_PALACE';
-
-INSERT INTO "Building_Flavors" ('BuildingType', 'FlavorType',  'Flavor')
-	VALUES	("BUILDING_PALACE", "FLAVOR_PRODUCTION", "150");
-INSERT INTO "Building_Flavors" ('BuildingType', 'FlavorType',  'Flavor')
-	VALUES	("BUILDING_PALACE", "FLAVOR_EXPANSION", "150");
+INSERT INTO Building_Flavors	(BuildingType,			FlavorType,					Flavor) VALUES	
+								('BUILDING_PALACE',		'FLAVOR_PRODUCTION',		150),
+								('BUILDING_PALACE',		'FLAVOR_EXPANSION',			150);
 
